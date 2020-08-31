8249626a40e271d9961d91db776937aa49893f01
Branton Holmes <bholmes@sofi.org>
2020-08-31 17:30:08 -0600

    committing before running npm run eject in case everything blows up

--- src/People.test.js
+++ src/People.test.js
@@ -1,9 +1,55 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import moxios from 'moxios';
+import { render, waitFor, screen } from '@testing-library/react';
 import People from './People';
 
-test('Renders list of people returned from the SWAPI', () => {
-  const { getByText } = render(<People />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const swapiResponse = {
+  results: [
+  {
+    name: 'test mctestyface',
+    gender: 'male',
+    birth_year: 'long long ago',
+    height: '1',
+    mass: '2',
+    films: [],
+  },
+  {
+    name: 'john doe',
+    gender: 'doesn\'t matter',
+    birth_year: 'never',
+    height: '125',
+    mass: '12',
+    films: [],
+  },
+  {
+    name: 'Malla',
+    gender: 'female',
+    birth_year: 'Life Day',
+    height: '7',
+    mass: '300',
+    films: [],
+  },
+]};
+
+describe('Component: People', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('Renders list of people returned from the SWAPI', async () => {
+    render(<People />);
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: swapiResponse
+      });
+    });
+    await waitFor(() => screen.getByText('test mctestyface'));
+    const rows = await screen.findAllByRole('row');
+    expect(rows.length).toEqual(3);
+  })
 });
